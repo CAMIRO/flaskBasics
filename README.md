d7b946109dbac02f6fda98ebe72123ef4317675a
CAMIRO <juancamilosc@hotmail.com>
2021-11-28 16:45:09 -0500

    configuracion sesiones en Flask

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # flaskBasics
 
 ## Start & watch
-
 ```
     source venv/bin/activate
 
     export FLASK_APP=main.py
     export FLASK_DEBUG=1
+    export FLASK_ENV=development
 
     flask run
+```
+
+### Install dependencies
+```
+    pip install -r requierements.txt
 ```
 
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar cafe', 'TODO 2', 'TODO 3']
 
@@ -15,14 +17,15 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip',user_ip)
+    session['user_ip'] = user_ip
+    """ response.set_cookie('user_ip',user_ip) """
 
     return response
 
 
 @app.route('/hello')
 def hello():   
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     # Dictionary:
     context = {
         'user_ip': user_ip, 
